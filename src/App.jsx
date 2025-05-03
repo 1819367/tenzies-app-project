8707fbb9c7ca04d9ecb76344750f0904993e3d0c
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-02 17:43:00 -0700

    added number generator function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import './App.css'
 import Die from './components/Die'
 
 export default function App() {
+
+function generateAllNewDice() {
+  return new Array(10) //creates a new array of length 10
+    .fill(0) //fill it with zeros
+    .map(() => Math.ceil(Math.random() * 6)) ///map over the array,
+}
+
+console.log(generateAllNewDice()) //console log a call to generateAllNewDice()
+
   return (
     <main>
       <div className='dice-container'>
