96770a5c18c4e38c5e1aab90e0ec85e36d78b1ab
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-05 17:03:56 -0700

    generate array of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 import Die from './components/Die'
 
 export default function App() {
 
+// state to hold array of dice
+const [ dice, setDice ] = useState(generateAllNewDice());
+
 function generateAllNewDice() {
   return new Array(10) //creates a new array of length 10
     .fill(0) //fill it with zeros
-    .map(() => Math.ceil(Math.random() * 6)) ///map over the array,
+    .map(() => Math.ceil(Math.random() * 6)) ///map over the array
 }
 
-console.log(generateAllNewDice()) //console log a call to generateAllNewDice()
+//map over dice and for every die inside the array, render a Die component
+const diceElements = dice.map((die) => (
+    <Die value={die} />
+))
 
   return (
     <main>
       <div className='dice-container'>
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
+        {diceElements}
       </div>
     </main>
   )
