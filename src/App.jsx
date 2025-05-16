ce43f5691fd826415cebf967483aeb374245c355
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-16 16:52:30 -0700

    added gamewon variable

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,51 @@ import { nanoid } from 'nanoid'
 
 export default function App() {
 
-// state to hold array of dice
-const [ dice, setDice ] = useState(generateAllNewDice());
+  // state to hold array of dice
+  const [ dice, setDice ] = useState(generateAllNewDice());
 
-function generateAllNewDice() {
-  return new Array(10) //creates a new array of length 10
-    .fill(0) //fill it with zeros
-    .map(() => ({     ///map over the array, replace each zero and return an object
-      value: Math.ceil(Math.random() * 6), //value is a random number between 1 and 6.
-      isHeld: false, //isHeld is always false.
-      id: nanoid() //generate an id
-    }))
-}
 
-//generate new dice with button click and update the state
-function rollDice () {
-  setDice(prevDice => prevDice.map(die => //prevDice.map loops through each die
-    die.isHeld ? 
-      die : //if die.isHeld is true, return the die as is
-      {...die, value: Math.ceil(Math.random() * 6) } //If not, return a new object with all the properties, but a new random value
+  const allHeld = dice.every(die => die.isHeld); //true if all dice are held
+  const allSameValue = dice.every(die => die.value === dice[0].value); //true if all dice have the same value
+  const gameWon = (allHeld && allSameValue) //true if both conditions are true
+
+  function generateAllNewDice() {
+    return new Array(10) //creates a new array of length 10
+      .fill(0) //fill it with zeros
+      .map(() => ({     ///map over the array, replace each zero and return an object
+        value: Math.ceil(Math.random() * 6), //value is a random number between 1 and 6.
+        isHeld: false, //isHeld is always false.
+        id: nanoid() //generate an id
+      }))
+  }
+
+  //generate new dice with button click and update the state
+  function rollDice () {
+    setDice(prevDice => prevDice.map(die => //prevDice.map loops through each die
+      die.isHeld ? 
+        die : //if die.isHeld is true, return the die as is
+        {...die, value: Math.ceil(Math.random() * 6) } //If not, return a new object with all the properties, but a new random value
   ))}
   
-//refactored, update the hold function to flip 'isHeld' property on the object in the array that was clicked
-function hold(id) {
- setDice(prevDice => prevDice.map(item => 
-    item.id === id ? 
-      {...item, isHeld: !item.isHeld} :
-      item
- ))
-}
+  //refactored, update the hold function to flip 'isHeld' property on the object in the array that was clicked
+  function hold(id) {
+    setDice(prevDice => prevDice.map(item => 
+      item.id === id ? 
+        {...item, isHeld: !item.isHeld} :
+        item
+    ))
+  }
 
-//map over dice  array and for each die object render a Die component
-const diceElements = dice.map((dieObj) => (
+  //map over dice  array and for each die object render a Die component
+  const diceElements = dice.map((dieObj) => (
     <Die 
       key={dieObj.id} 
       value={dieObj.value} //pass value and id properties of each die as a props
       isHeld={dieObj.isHeld} //pass isHeld property of each die as a props
       id={dieObj.id} //pass the id
       hold={hold} //pass the hold function
-      />
-))
+    />
+  ))
 
   return (
     <main>
@@ -59,9 +64,9 @@ const diceElements = dice.map((dieObj) => (
         className='btn-roll btn-roll_style'
         onClick={() => rollDice()}  
         >
-            Roll
+           {gameWon ? "New Game" : "Roll" }  
       </button>
     </main>
   )
-};
+}
 
