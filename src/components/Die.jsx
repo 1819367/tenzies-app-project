b5ec25ef0f2c0763ed192598aa767ed5ba98b778
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-21 14:57:29 -0700

    added accessibility improvements

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,10 @@ export default function Die(props) {
     return  (
     <button 
        className={`btn-die ${props.isHeld ? "btn-die_held" : "btn-die__notheld"}`}
-       onClick={() => props.hold(props.id)}> 
+       onClick={() => props.hold(props.id)}
+       aria-pressed={props.isHeld} // lets the player know the the current pressed state
+       aria-label={`Die with a value of ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+        > 
             {props.value}
         </button>
     )
